3e3b972d0e617d622a254153a8037a8a82f1d6ad
fraxouille <fraxouille@hotmail.com>
2025-06-09 17:36:25 +0200

    asteroids

--- Character/Enemy/asteroid.gd
+++ Character/Enemy/asteroid.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 1
+@export var speed = 100
+@export var rotationSpeed = 0.1
 @export var damage = 1
 @export var life = 1
+@export var orientation = 0.6
+var direction
+
+func _ready():
+	rotationSpeed = randf_range(-rotationSpeed, rotationSpeed)
+	direction = Vector2(randf_range(-orientation, orientation), 1).normalized()
 
 func _physics_process(delta: float) -> void:
-	position.y += speed
+	velocity = direction * speed
+	rotation += rotationSpeed
+	move_and_slide()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
